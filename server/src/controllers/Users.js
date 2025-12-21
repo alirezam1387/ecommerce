1a9e73b1f4721847ce95ff785686ffa14ce868ac
Alireza <alirezam21359640@gmail.com>
2025-12-21 23:34:24 +0330

    err fixer

--- server/src/controllers/Users.js
+++ server/src/controllers/Users.js
@@ -2,10 +2,11 @@ const AsyncHandler = require("../utils/asyncHandller");
 const ErrorHandler = require("../middlewere/ErrorHandler")
 
 const register = AsyncHandler((req, res, next) => {
+    console.log(res)
     return next(new ErrorHandler('email is wrong', 400))
 
 })
 const login = () => { };
 const update = () => { };
 const logout = () => { };
-const forgotPassword = () => { };
+const forgotPassword = () => {};
