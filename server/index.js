13ffc1164e96d362e6fc95efbda9633566fddac9
Parsa <business.parsa.taheri@gmail.com>
2025-12-21 14:28:17 +0330

    setup server with express

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+require("dotenv").config({
+  quiet: true,
+});
+
+const server = express();
+
+server.use(express.json());
+server.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+server.use(cors());
+
+const PORT = process.env.PORT || 5050; // default port for server is 5050
+
+server.listen(PORT, () => console.log(`server is running on port ${PORT}`));
