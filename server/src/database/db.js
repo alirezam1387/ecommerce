28f3b6e1c103d5f8a437765cb09195a0a5a95c63
Parsa <business.parsa.taheri@gmail.com>
2025-12-21 16:30:18 +0330

    fix

--- server/src/database/db.js
+++ server/src/database/db.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const ConnectDB = async () => {
   try {
-    console.log(process.env.MONGO_URI);
     await mongoose.connect(process.env.MONGO_URI);
     console.log("database connect successfuly");
   } catch (err) {
